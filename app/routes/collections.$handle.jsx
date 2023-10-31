b29a2ca3cf779e3bb62a7b6262c1b0d23fa609bf
HexTreeStudio <lukasplesnik@gmail.com>
2023-10-31 11:53:07 +0100

    all products

--- app/routes/collections.$handle.jsx
+++ app/routes/collections.$handle.jsx
@@ -1,197 +1,102 @@
-import {json, redirect} from '@shopify/remix-oxygen';
-import {useLoaderData, Link} from '@remix-run/react';
-import {
-  Pagination,
-  getPaginationVariables,
-  Image,
-  Money,
-} from '@shopify/hydrogen';
-import {useVariantUrl} from '~/utils';
+import { useLoaderData } from '@remix-run/react';
+import { json } from '@shopify/remix-oxygen';
+import ProductCard from '~/components/ProductCard';
 
-/**
- * @type {V2_MetaFunction}
- */
-export const meta = ({data}) => {
-  return [{title: `Hydrogen | ${data.collection.title} Collection`}];
+import ProductGrid from '../components/ProductGrid';
+
+export const handle = {
+  seo,
 };
 
-/**
- * @param {LoaderArgs}
- */
-export async function loader({request, params, context}) {
-  const {handle} = params;
-  const {storefront} = context;
-  const paginationVariables = getPaginationVariables(request, {
-    pageBy: 8,
-  });
+function seo({ data }) {
+  return {
+    title: data?.collection?.title,
+    description: data?.collection?.description,
+  };
+}
 
-  if (!handle) {
-    return redirect('/collections');
-  }
+export async function loader({ params, context }) {
+  const { handle } = params;
 
-  const {collection} = await storefront.query(COLLECTION_QUERY, {
-    variables: {handle, ...paginationVariables},
-  });
+  const productsPerPage = 250; // Počet produktů na stránku
+  let products = [];
+  let cursor = null;
 
-  if (!collection) {
-    throw new Response(`Collection ${handle} not found`, {
-      status: 404,
+  while (true) {
+    const { collection } = await context.storefront.query(COLLECTION_QUERY, {
+      variables: {
+        handle,
+        first: productsPerPage,
+        after: cursor,
+      },
     });
+
+    if (!collection) {
+      throw new Response(null, { status: 404 });
+    }
+
+    products = products.concat(collection.products.nodes);
+
+    if (!collection.products.pageInfo.hasNextPage) {
+      break; // Zastavit načítání, pokud není další stránka
+    }
+
+    cursor = collection.products.pageInfo.endCursor;
   }
-  return json({collection});
+
+  return json({
+    products,
+  });
 }
 
 export default function Collection() {
-  /** @type {LoaderReturnData} */
-  const {collection} = useLoaderData();
-
+  const { products } = useLoaderData();
+console.log(products);
   return (
-    <div className="collection">
-      <h1>{collection.title}</h1>
-      <p className="collection-description">{collection.description}</p>
-      <Pagination connection={collection.products}>
-        {({nodes, isLoading, PreviousLink, NextLink}) => (
-          <>
-            <PreviousLink>
-              {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
-            </PreviousLink>
-            <ProductsGrid products={nodes} />
-            <br />
-            <NextLink>
-              {isLoading ? 'Loading...' : <span>Load more ↓</span>}
-            </NextLink>
-          </>
-        )}
-      </Pagination>
+    <div>
+      <ProductGrid products={products}/>
+
     </div>
   );
 }
 
-/**
- * @param {{products: ProductItemFragment[]}}
- */
-function ProductsGrid({products}) {
-  return (
-    <div className="products-grid">
-      {products.map((product, index) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            loading={index < 8 ? 'eager' : undefined}
-          />
-        );
-      })}
-    </div>
-  );
-}
-
-/**
- * @param {{
- *   product: ProductItemFragment;
- *   loading?: 'eager' | 'lazy';
- * }}
- */
-function ProductItem({product, loading}) {
-  const variant = product.variants.nodes[0];
-  const variantUrl = useVariantUrl(product.handle, variant.selectedOptions);
-  return (
-    <Link
-      className="product-item"
-      key={product.id}
-      prefetch="intent"
-      to={variantUrl}
-    >
-      {product.featuredImage && (
-        <Image
-          alt={product.featuredImage.altText || product.title}
-          aspectRatio="1/1"
-          data={product.featuredImage}
-          loading={loading}
-          sizes="(min-width: 45em) 400px, 100vw"
-        />
-      )}
-      <h4>{product.title}</h4>
-      <small>
-        <Money data={product.priceRange.minVariantPrice} />
-      </small>
-    </Link>
-  );
-}
-
-const PRODUCT_ITEM_FRAGMENT = `#graphql
-  fragment MoneyProductItem on MoneyV2 {
-    amount
-    currencyCode
-  }
-  fragment ProductItem on Product {
-    id
-    handle
-    title
-    featuredImage {
-      id
-      altText
-      url
-      width
-      height
-    }
-    priceRange {
-      minVariantPrice {
-        ...MoneyProductItem
-      }
-      maxVariantPrice {
-        ...MoneyProductItem
-      }
-    }
-    variants(first: 1) {
-      nodes {
-        selectedOptions {
-          name
-          value
-        }
-      }
-    }
-  }
-`;
-
-// NOTE: https://shopify.dev/docs/api/storefront/2022-04/objects/collection
 const COLLECTION_QUERY = `#graphql
-  ${PRODUCT_ITEM_FRAGMENT}
-  query Collection(
-    $handle: String!
-    $country: CountryCode
-    $language: LanguageCode
-    $first: Int
-    $last: Int
-    $startCursor: String
-    $endCursor: String
-  ) @inContext(country: $country, language: $language) {
+  query CollectionDetails($handle: String!, $first: Int!, $after: String) {
     collection(handle: $handle) {
       id
-      handle
       title
       description
-      products(
-        first: $first,
-        last: $last,
-        before: $startCursor,
-        after: $endCursor
-      ) {
+      products(first: $first, after: $after) {
         nodes {
-          ...ProductItem
+          id
+          title
+          publishedAt
+          handle
+          variants(first: 1) {
+            nodes {
+              id
+              image {
+                url
+                altText
+                width
+                height
+              }
+              price {
+                amount
+                currencyCode
+              }
+              compareAtPrice {
+                amount
+                currencyCode
+              }
+            }
+          }
         }
         pageInfo {
-          hasPreviousPage
           hasNextPage
           endCursor
-          startCursor
         }
       }
     }
   }
 `;
-
-/** @typedef {import('@shopify/remix-oxygen').LoaderArgs} LoaderArgs */
-/** @template T @typedef {import('@remix-run/react').V2_MetaFunction<T>} V2_MetaFunction */
-/** @typedef {import('storefrontapi.generated').ProductItemFragment} ProductItemFragment */
-/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
