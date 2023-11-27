0214cf55d97fec8f08b1a389c7e9bbde541a0a23
HexTreeStudio <lukasplesnik@gmail.com>
2023-11-27 12:00:25 +0100

    for all collections

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -33,7 +33,7 @@ export default function Index() {
 
 const COLLECTIONS_QUERY = `#graphql
   query FeaturedCollections {
-    collections(first: 3, query: "collection_type:smart") {
+    collections(first: 200, query: "collection_type:smart") {
       nodes {
         id
         title
